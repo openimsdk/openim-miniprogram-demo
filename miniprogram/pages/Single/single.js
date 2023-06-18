81878ed5334c627f6d07060ac55b933f49bba185
AllianceTing <2426446427@qq.com>
2023-06-18 23:31:52 +0800

    [tree] 完成基础界面单人/多人的样式编写

---
+++ miniprogram/pages/Single/single.js
@@ -0,0 +1,28 @@
+Page({
+  data: {
+    messages: [],        // 消息列表
+    inputValue: ''      // 输入框内容
+  },
+
+  inputChange: function(e) {
+    this.setData({
+      inputValue: e.detail.value
+    });
+  },
+  sendMessage: function() {
+    var newMessage = {
+      sender: 'AllianceTing',
+      text: this.data.inputValue,
+      avatarUrl:"/images/avatar2.png"
+    };
+    var messages = this.data.messages;
+    if(messages.text == "") return;
+    else{
+      messages.push(newMessage);
+    }
+    this.setData({
+      messages: messages,
+      inputValue: ''
+    });
+  }
+});
---
+++ workDirary.md
@@ -0,0 +1,24 @@
+#### mini-program(demo) v0.0.1 Working dirary
+
+​		day 1:
+
+- create baseRepo https://github.com/AllianceTing/openim-miniprogram-demo
+
+- edit Single(单人聊天页面） — js
+
+- edit Chat(多人聊天/群聊页面) —-js
+
+  ------
+
+  day2:
+
+  - edit Contcat(通讯录页面) 
+  - edit navigationBar  —- icon
+  - edit TabBar   —- icon 
+
+  needs to do:
+
+  - [ ] 抽离基础样式封装成组件形成复用体系
+
+  
+
