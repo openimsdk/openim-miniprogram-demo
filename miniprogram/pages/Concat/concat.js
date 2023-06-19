fe9f8540de4a366f8ab177acf3dd3a425ba4326f
AllianceTing <2426446427@qq.com>
2023-06-19 20:23:26 +0800

    [tree] 完成了个人中心页面，通讯录页面的搭建

--- miniprogram/pages/Concat/concat.js
+++ miniprogram/pages/Concat/concat.js
@@ -1,8 +1,9 @@
 Page({
   data: {
+    letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '#'],
     contacts: [
       {
-        avatar: "/images/avatar1.png",
+        avatar: "/images/avatar2.png",
         name: "张三",
         phone: "13712345678"
       },
@@ -12,10 +13,22 @@ Page({
         phone: "13898765432"
       },
       {
-        avatar: "/images/avatar3.png",
+        avatar: "/images/avatar2.png",
         name: "王五",
         phone: "13987654321"
       }
-    ]
+    ],
+    searchResult: []
+  },
+  onInputChange: function(e) {
+    this.setData({
+      searchResult: [] // 清空搜索结果
+    });
+  },
+  onSearch: function() {
+    // 在实际应用中，可以进行搜索请求，将搜索结果存储在searchResult数组中
+    this.setData({
+      searchResult: ['搜索结果1', '搜索结果2', '搜索结果3']
+    });
   }
 })
