6180394b9513610632d79dd46bb2dc8a71b4a8ef
Bloomingg <1293499952@qq.com>
2023-06-25 10:08:54 +0800

    feat: im sdk access

--- miniprogram/pages/Index/index.js
+++ miniprogram/pages/Index/index.js
@@ -1,3 +1,7 @@
+import { CbEvents, OpenIMSDK } from "open-im-sdk";
+
+const im = new OpenIMSDK();
+
 Page({
   data: {
     messages: [
@@ -17,6 +21,28 @@ Page({
     ],
     nbFrontColor: '#000000',
     nbBackgroundColor: '#ffffff',
+  },
+  imLogin(){
+    const options = {
+      userID:"940974738",
+      token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVSUQiOiI5NDA5NzQ3MzgiLCJQbGF0Zm9ybSI6IldlYiIsImV4cCI6MTY5NTAyNDg2NSwibmJmIjoxNjg3MjQ4NTY1LCJpYXQiOjE2ODcyNDg4NjV9.4t4f5VtiXLWdj0TfHIRvbNj_45HFqjWsBI3dMrbkflY",
+      url:"ws://121.5.182.23:10003",
+      platformID:5,
+    }
+    im.login(options).then(res=>{
+      console.log(res);
+    }).catch(err=>{
+      console.log(err);
+    })
+    im.on(CbEvents.ONRECVNEWMESSAGE,()=>{
+      console.log("new msg::::::");
+    })
+    im.on(CbEvents.ONRECVC2CREADRECEIPT,()=>{
+      console.log("c2c msg:::::");
+    })
+    im.on(CbEvents.ONTOTALUNREADMESSAGECOUNTCHANGED,()=>{
+      console.log("total change::::::");
+    })
   },
   onLoad() {
     this.setData({
